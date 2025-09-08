1526b99a77de276972df9ff6b03296b0d3d38028
Masahiro Nakagawa <repeatedly@gmail.com>
2025-09-08 16:07:50 +0900

    Use beta version
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ import "fmt"
 
 var (
 	// Version app version
-	Version = "3.0.0-dev.1"
+	Version = "3.0.0-beta1"
 	// Revision git commit short commithash
 	Revision = "xxxxxx" // set on build
 )
