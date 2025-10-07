39f1b95a6aa04653d3236ae9dcbd217d2acae0a2
Masahiro Nakagawa <repeatedly@gmail.com>
2025-10-07 09:12:36 +0900

    v3.0.0-beta3
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ import "fmt"
 
 var (
 	// Version app version
-	Version = "3.0.0-beta1"
+	Version = "3.0.0-beta3"
 	// Revision git commit short commithash
 	Revision = "xxxxxx" // set on build
 )
