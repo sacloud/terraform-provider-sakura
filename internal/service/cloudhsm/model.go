1f72be1893a73d905649f4a2f3f6f5fd11581901
Masahiro Nakagawa <repeatedly@gmail.com>
2025-11-21 12:50:41 +0900

    Add CloudHSM data sources and resources
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

---
+++ internal/service/cloudhsm/model.go
@@ -0,0 +1,105 @@
+// Copyright 2016-2025 The terraform-provider-sakura Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package cloudhsm
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	v1 "github.com/sacloud/cloudhsm-api-go/apis/v1"
+	"github.com/sacloud/terraform-provider-sakura/internal/common"
+)
+
+type cloudHSMBaseModel struct {
+	common.SakuraBaseModel
+	CreatedAt          types.String `tfsdk:"created_at"`
+	ModifiedAt         types.String `tfsdk:"modified_at"`
+	Availability       types.String `tfsdk:"availability"`
+	IPv4NetworkAddress types.String `tfsdk:"ipv4_network_address"`
+	IPv4Netmask        types.Int32  `tfsdk:"ipv4_netmask"`
+	IPv4Address        types.String `tfsdk:"ipv4_address"`
+	LocalRouter        types.Object `tfsdk:"local_router"`
+}
+
+type cloudHSMLocalRouterModel struct {
+	ID        types.String `tfsdk:"id"`
+	SecretKey types.String `tfsdk:"secret_key"`
+}
+
+func (m cloudHSMLocalRouterModel) AttributeTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"id":         types.StringType,
+		"secret_key": types.StringType,
+	}
+}
+
+func (model *cloudHSMBaseModel) updateState(cloudhsm *v1.CloudHSM) {
+	model.UpdateBaseState(cloudhsm.ID, cloudhsm.Name, cloudhsm.Description.Value, cloudhsm.Tags)
+	model.CreatedAt = types.StringValue(string(cloudhsm.CreatedAt))
+	model.ModifiedAt = types.StringValue(string(cloudhsm.ModifiedAt))
+	model.Availability = types.StringValue(string(cloudhsm.Availability))
+	model.IPv4NetworkAddress = types.StringValue(cloudhsm.Ipv4NetworkAddress)
+	model.IPv4Netmask = types.Int32Value(int32(cloudhsm.Ipv4PrefixLength))
+	model.IPv4Address = types.StringValue(cloudhsm.Ipv4Address)
+
+	if router, ok := cloudhsm.LocalRouter.Get(); ok {
+		m := cloudHSMLocalRouterModel{
+			ID:        types.StringValue(router.ResourceID.Value),
+			SecretKey: types.StringValue(router.SecretKey.Value),
+		}
+		value, _ := types.ObjectValueFrom(context.Background(), m.AttributeTypes(), m)
+		model.LocalRouter = value
+	} else {
+		model.LocalRouter = types.ObjectNull(cloudHSMLocalRouterModel{}.AttributeTypes())
+	}
+}
+
+type cloudHSMClientBaseModel struct {
+	ID           types.String `tfsdk:"id"`
+	Name         types.String `tfsdk:"name"`
+	CloudHSMID   types.String `tfsdk:"cloudhsm_id"`
+	Certificate  types.String `tfsdk:"certificate"`
+	CreatedAt    types.String `tfsdk:"created_at"`
+	ModifiedAt   types.String `tfsdk:"modified_at"`
+	Availability types.String `tfsdk:"availability"`
+}
+
+func (model *cloudHSMClientBaseModel) updateState(cloudhsmClient *v1.CloudHSMClient, cloudhsmId string) {
+	model.ID = types.StringValue(cloudhsmClient.ID)
+	model.Name = types.StringValue(cloudhsmClient.Name)
+	model.CloudHSMID = types.StringValue(cloudhsmId)
+	model.Certificate = types.StringValue(cloudhsmClient.Certificate)
+	model.CreatedAt = types.StringValue(string(cloudhsmClient.CreatedAt))
+	model.ModifiedAt = types.StringValue(string(cloudhsmClient.ModifiedAt))
+	model.Availability = types.StringValue(string(cloudhsmClient.Availability))
+}
+
+type cloudHSMPeerBaseModel struct {
+	ID         types.String `tfsdk:"id"`
+	CloudHSMID types.String `tfsdk:"cloudhsm_id"`
+	Index      types.Int64  `tfsdk:"index"`
+	Status     types.String `tfsdk:"status"`
+	Routes     types.List   `tfsdk:"routes"`
+}
+
+func (model *cloudHSMPeerBaseModel) updateState(peer *v1.CloudHSMPeer, cloudhsmId string) {
+	model.ID = types.StringValue(peer.ID)
+	model.Index = types.Int64Value(int64(peer.Index.Value))
+	model.Status = types.StringValue(string(peer.Status.Value))
+	model.Routes = common.StringsToTlist(peer.Routes)
+	model.CloudHSMID = types.StringValue(cloudhsmId)
+}
+
+type cloudHSMLicenseBaseModel struct {
+	common.SakuraBaseModel
+	CreatedAt  types.String `tfsdk:"created_at"`
+	ModifiedAt types.String `tfsdk:"modified_at"`
+}
+
+func (model *cloudHSMLicenseBaseModel) updateState(license *v1.CloudHSMSoftwareLicense) {
+	model.UpdateBaseState(license.ID, license.Name, license.Description, license.Tags)
+	model.CreatedAt = types.StringValue(string(license.CreatedAt))
+	model.ModifiedAt = types.StringValue(string(license.ModifiedAt))
+}
---
+++ internal/service/cloudhsm/resource_client.go
@@ -0,0 +1,211 @@
+// Copyright 2016-2025 The terraform-provider-sakura Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package cloudhsm
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+
+	api "github.com/sacloud/api-client-go"
+	"github.com/sacloud/cloudhsm-api-go"
+	v1 "github.com/sacloud/cloudhsm-api-go/apis/v1"
+	"github.com/sacloud/terraform-provider-sakura/internal/common"
+	sacloudvalidator "github.com/sacloud/terraform-provider-sakura/internal/validator"
+)
+
+type cloudHSMClientResource struct {
+	client *v1.Client
+}
+
+var (
+	_ resource.Resource                = &cloudHSMClientResource{}
+	_ resource.ResourceWithConfigure   = &cloudHSMClientResource{}
+	_ resource.ResourceWithImportState = &cloudHSMClientResource{}
+)
+
+func NewCloudHSMClientResource() resource.Resource {
+	return &cloudHSMClientResource{}
+}
+
+func (r *cloudHSMClientResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_cloudhsm_client"
+}
+
+func (r *cloudHSMClientResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	apiclient := common.GetApiClientFromProvider(req.ProviderData, &resp.Diagnostics)
+	if apiclient == nil {
+		return
+	}
+	r.client = apiclient.CloudHSMClient
+}
+
+type cloudHSMClientResourceModel struct {
+	cloudHSMClientBaseModel
+	Timeouts timeouts.Value `tfsdk:"timeouts"`
+}
+
+func (r *cloudHSMClientResource) Schema(ctx context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id":   common.SchemaResourceId("CloudHSM Client"),
+			"name": common.SchemaResourceName("CloudHSM Client"),
+			"cloudhsm_id": schema.StringAttribute{
+				Required:    true,
+				Description: "The ID of the CloudHSM to associate with the client",
+				Validators: []validator.String{
+					sacloudvalidator.SakuraIDValidator(),
+				},
+			},
+			"certificate": schema.StringAttribute{
+				Required:    true,
+				Description: "The certificate for the CloudHSM Client",
+			},
+			"created_at": schema.StringAttribute{
+				Computed:    true,
+				Description: "The creation date of the CloudHSM Client",
+			},
+			"modified_at": schema.StringAttribute{
+				Computed:    true,
+				Description: "The modification date of the CloudHSM Client",
+			},
+			"availability": schema.StringAttribute{
+				Computed:    true,
+				Description: "The availability status of the CloudHSM Client",
+			},
+			"timeouts": timeouts.Attributes(ctx, timeouts.Opts{
+				Create: true, Update: true, Delete: true,
+			}),
+		},
+		MarkdownDescription: "Manages a CloudHSM Client.",
+	}
+}
+
+func (r *cloudHSMClientResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func (r *cloudHSMClientResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan cloudHSMClientResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := common.SetupTimeoutCreate(ctx, plan.Timeouts, common.Timeout20min)
+	defer cancel()
+
+	chsm := getCloudHSM(ctx, r.client, plan.CloudHSMID.ValueString(), &resp.State, &resp.Diagnostics)
+	if chsm == nil {
+		return
+	}
+
+	clientOp, _ := cloudhsm.NewClientOp(r.client, chsm)
+	created, err := clientOp.Create(ctx, cloudhsm.CloudHSMClientCreateParams{
+		Name:        plan.Name.ValueString(),
+		Certificate: plan.Certificate.ValueString(),
+	})
+	if err != nil {
+		resp.Diagnostics.AddError("Create Error", err.Error())
+		return
+	}
+
+	plan.updateState(created, chsm.ID)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (r *cloudHSMClientResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state cloudHSMClientResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	chsm := getCloudHSM(ctx, r.client, state.CloudHSMID.ValueString(), &resp.State, &resp.Diagnostics)
+	if chsm == nil {
+		return
+	}
+
+	chsmClient := getCloudHSMClient(ctx, r.client, chsm, state.ID.ValueString(), &resp.State, &resp.Diagnostics)
+	if chsmClient == nil {
+		return
+	}
+
+	state.updateState(chsmClient, chsm.ID)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *cloudHSMClientResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan cloudHSMClientResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := common.SetupTimeoutUpdate(ctx, plan.Timeouts, common.Timeout5min)
+	defer cancel()
+
+	chsm := getCloudHSM(ctx, r.client, plan.CloudHSMID.ValueString(), &resp.State, &resp.Diagnostics)
+	if chsm == nil {
+		return
+	}
+
+	clientOp, _ := cloudhsm.NewClientOp(r.client, chsm)
+	updated, err := clientOp.Update(ctx, plan.ID.ValueString(), cloudhsm.CloudHSMClientUpdateParams{Name: plan.Name.ValueString()})
+	if err != nil {
+		resp.Diagnostics.AddError("Update Error", err.Error())
+		return
+	}
+
+	plan.updateState(updated, chsm.ID)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (r *cloudHSMClientResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state cloudHSMClientResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := common.SetupTimeoutDelete(ctx, state.Timeouts, common.Timeout5min)
+	defer cancel()
+
+	chsm := getCloudHSM(ctx, r.client, state.CloudHSMID.ValueString(), &resp.State, &resp.Diagnostics)
+	if chsm == nil {
+		return
+	}
+
+	clientOp, err := cloudhsm.NewClientOp(r.client, chsm)
+	if err != nil {
+		resp.Diagnostics.AddError("Delete Error", err.Error())
+		return
+	}
+
+	if err := clientOp.Delete(ctx, state.ID.ValueString()); err != nil {
+		resp.Diagnostics.AddError("Delete Error", err.Error())
+		return
+	}
+}
+
+func getCloudHSMClient(ctx context.Context, client *v1.Client, chsm *v1.CloudHSM, id string, state *tfsdk.State, diags *diag.Diagnostics) *v1.CloudHSMClient {
+	clientOp, _ := cloudhsm.NewClientOp(client, chsm)
+	chsmClient, err := clientOp.Read(ctx, id)
+	if err != nil {
+		if api.IsNotFoundError(err) {
+			state.RemoveResource(ctx)
+			return nil
+		}
+		diags.AddError("Get CloudHSM Client Error", fmt.Sprintf("failed to read CloudHSM Client[%s]: %s", id, err))
+		return nil
+	}
+	return chsmClient
+}
