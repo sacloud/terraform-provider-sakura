f4e09fc8666d9a0dfb50aaca2099201fcc802b0f
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2025-12-26 14:50:01 +0900

    Refactor Makefile targets and add generate-docs target
    Signed-off-by: Kazumichi Yamamoto <yamamoto.febc@gmail.com>

--- tools/tools.go
+++ tools/tools.go
@@ -1,4 +1,4 @@
-//go:build tools
+//go:build generate
 
 package tools
 
@@ -6,3 +6,6 @@ import (
 	// Documentation generation
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
+
+// Generate documentation.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir ..  --provider-name=sakura
