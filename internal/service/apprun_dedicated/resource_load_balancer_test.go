2d6a055a5a5e24f57dce5c12f9d71ddb1a08283e
卜部 昌平 <s-urabe@sakura.ad.jp>
2026-03-31 13:55:36 +0900

    acceptance test for apprun_dedicated_load_balancer
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

---
+++ internal/service/apprun_dedicated/resource_load_balancer_test.go
@@ -0,0 +1,171 @@
+// Copyright 2016-2026 The terraform-provider-sakura Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package apprun_dedicated_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	lb "github.com/sacloud/apprun-dedicated-api-go/apis/loadbalancer"
+	v1 "github.com/sacloud/apprun-dedicated-api-go/apis/v1"
+	"github.com/sacloud/terraform-provider-sakura/internal/test"
+)
+
+func TestAccSakuraResourceApprunDedicatedLoadBalancer(t *testing.T) {
+	resourceName := "sakura_apprun_dedicated_load_balancer.main"
+	name := acctest.RandStringFromCharSet(14, acctest.CharSetAlphaNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 AccPreCheck(t),
+		ProtoV6ProviderFactories: test.AccProtoV6ProviderFactories,
+		CheckDestroy:             testCheckSakuraApprunDedicatedLoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: test.BuildConfigWithArgs(testAccSakuraResourceApprunDedicatedLoadBalancer_basic, name, globalClusterID),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("cluster_id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("name"), knownvalue.StringExact("tfacc-"+name)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("auto_scaling_group_id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("service_class_path"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("interfaces"), knownvalue.ListExact([]knownvalue.Check{
+						knownvalue.ObjectExact(map[string]knownvalue.Check{
+							"interface_index":   knownvalue.Int32Exact(0),
+							"upstream":          knownvalue.StringRegexp(regexp.MustCompile("^[0-9]+$")),
+							"default_gateway":   knownvalue.NotNull(),
+							"netmask_len":       knownvalue.NotNull(),
+							"vip":               knownvalue.NotNull(),
+							"virtual_router_id": knownvalue.Int32Exact(1),
+							"packet_filter_id":  knownvalue.Null(),
+							"ip_pool": knownvalue.ListExact([]knownvalue.Check{
+								knownvalue.ObjectExact(map[string]knownvalue.Check{
+									"start": knownvalue.NotNull(),
+									"end":   knownvalue.NotNull(),
+								}),
+							}),
+						}),
+					})),
+				},
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"timeouts", "created", "deleting"},
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					rs, ok := s.RootModule().Resources[resourceName]
+					if !ok {
+						return "", fmt.Errorf("not found: %s", resourceName)
+					}
+					clusterID := rs.Primary.Attributes["cluster_id"]
+					autoScalingGroupID := rs.Primary.Attributes["auto_scaling_group_id"]
+					id := rs.Primary.Attributes["id"]
+					return fmt.Sprintf("%s/%s/%s", clusterID, autoScalingGroupID, id), nil
+				},
+			},
+		},
+	})
+}
+
+func testCheckSakuraApprunDedicatedLoadBalancerDestroy(s *terraform.State) error {
+	client := test.AccClientGetter().AppRunDedicatedClient
+	if client == nil {
+		return errors.New("AppRunDedicatedClient is nil")
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "sakura_apprun_dedicated_load_balancer" {
+			continue
+		}
+		if rs.Primary.ID == "" {
+			continue
+		}
+		clusterID, err := uuid.Parse(rs.Primary.Attributes["cluster_id"])
+		if err != nil {
+			return fmt.Errorf("invalid cluster ID: %s", rs.Primary.Attributes["cluster_id"])
+		}
+		autoScalingGroupID, err := uuid.Parse(rs.Primary.Attributes["auto_scaling_group_id"])
+		if err != nil {
+			return fmt.Errorf("invalid auto scaling group ID: %s", rs.Primary.Attributes["auto_scaling_group_id"])
+		}
+		lbID, err := uuid.Parse(rs.Primary.Attributes["id"])
+		if err != nil {
+			return fmt.Errorf("invalid load balancer ID: %s", rs.Primary.Attributes["id"])
+		}
+		api := lb.NewLoadBalancerOp(client, v1.ClusterID(clusterID), v1.AutoScalingGroupID(autoScalingGroupID))
+		_, err = api.Read(context.Background(), v1.LoadBalancerID(lbID))
+		if err == nil {
+			return fmt.Errorf("load balancer still exists: %s", rs.Primary.ID)
+		}
+	}
+	return nil
+}
+
+var testAccSakuraResourceApprunDedicatedLoadBalancer_basic = `
+locals {
+  sakura_dns = [ "133.242.0.3", "133.242.0.4" ]
+}
+
+data "sakura_zone" "is1c" {
+  name = "is1c"
+}
+
+data "sakura_apprun_dedicated_worker_service_classes" "main" {}
+
+resource "sakura_internet" "main" {
+  name = "tfacc-{{ .arg0 }}"
+  zone = data.sakura_zone.is1c.name
+}
+
+resource "sakura_apprun_dedicated_auto_scaling_group" "main" {
+  cluster_id                = "{{ .arg1 }}"
+  name                      = "tfacc-{{ .arg0 }}"
+  zone                      = data.sakura_zone.is1c.name
+  worker_service_class_path = data.sakura_apprun_dedicated_worker_service_classes.main.classes[0].path
+  min_nodes                 = 3
+  max_nodes                 = 7
+  interfaces = [
+    {
+      interface_index = 0
+      upstream        = "shared"
+      connects_to_lb  = true
+    }
+  ]
+}
+
+data "sakura_apprun_dedicated_load_balancer_service_classes" "main" {}
+
+resource "sakura_apprun_dedicated_load_balancer" "main" {
+  cluster_id            = sakura_apprun_dedicated_auto_scaling_group.main.cluster_id
+  auto_scaling_group_id = sakura_apprun_dedicated_auto_scaling_group.main.id
+  name                  = "tfacc-{{ .arg0 }}"
+  service_class_path    = data.sakura_apprun_dedicated_load_balancer_service_classes.main.classes[0].path
+  name_servers          = local.sakura_dns
+  interfaces = [
+    {
+      interface_index   = 0
+      upstream          = sakura_internet.main.vswitch_id
+      ip_pool = [
+        {
+          start = sakura_internet.main.min_ip_address
+          end   = sakura_internet.main.max_ip_address
+        }
+      ]
+      netmask_len       = sakura_internet.main.netmask
+      default_gateway   = sakura_internet.main.gateway
+      vip               = cidrhost("${sakura_internet.main.gateway}/${sakura_internet.main.netmask}", 9)
+      virtual_router_id = 1
+    }
+  ]
+}
+`
