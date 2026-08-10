eadeb05c0ed2ebe48d0d9900ff292fa958a2f33a
Masahiro Nakagawa <repeatedly@gmail.com>
2026-08-10 12:35:03 +0900

    Add SRN value and type
    Signed-off-by: Masahiro Nakagawa <repeatedly@gmail.com>

---
+++ internal/types/srn_type_test.go
@@ -0,0 +1,71 @@
+// Copyright 2016-2026 The terraform-provider-sakura Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package types_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/sacloud/terraform-provider-sakura/internal/types"
+)
+
+func TestSRNTypeValueFromTerraform(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		in          tftypes.Value
+		expectation attr.Value
+		expectedErr string
+	}{
+		"true": {
+			in:          tftypes.NewValue(tftypes.String, "srnv1:is1c:sakura.networking-suite.subnet:112233445566"),
+			expectation: types.SRNValue("srnv1:is1c:sakura.networking-suite.subnet:112233445566"),
+		},
+		"unknown": {
+			in:          tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
+			expectation: types.SRNUnknown(),
+		},
+		"null": {
+			in:          tftypes.NewValue(tftypes.String, nil),
+			expectation: types.SRNNull(),
+		},
+		"wrongType": {
+			in:          tftypes.NewValue(tftypes.Number, 123),
+			expectedErr: "can't unmarshal tftypes.Number into *string, expected string",
+		},
+	}
+	for name, testCase := range testCases {
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+
+			got, err := types.SRNType.ValueFromTerraform(ctx, testCase.in)
+			if err != nil {
+				if testCase.expectedErr == "" {
+					t.Fatalf("Unexpected error: %s", err)
+				}
+				if testCase.expectedErr != err.Error() {
+					t.Fatalf("Expected error to be %q, got %q", testCase.expectedErr, err.Error())
+				}
+				return
+			}
+			if testCase.expectedErr != "" {
+				t.Fatalf("Expected error to be %q, didn't get an error", testCase.expectedErr)
+			}
+			if !got.Equal(testCase.expectation) {
+				t.Errorf("Expected %+v, got %+v", testCase.expectation, got)
+			}
+			if testCase.expectation.IsNull() != testCase.in.IsNull() {
+				t.Errorf("Expected null-ness match: expected %t, got %t", testCase.expectation.IsNull(), testCase.in.IsNull())
+			}
+			if testCase.expectation.IsUnknown() != !testCase.in.IsKnown() {
+				t.Errorf("Expected unknown-ness match: expected %t, got %t", testCase.expectation.IsUnknown(), !testCase.in.IsKnown())
+			}
+		})
+	}
+}
